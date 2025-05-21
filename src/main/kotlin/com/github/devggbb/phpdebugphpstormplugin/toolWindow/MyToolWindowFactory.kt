f541a50210c89c5054f09c91647ae90021af0338
GitHub Action <action@github.com>
2025-05-21 12:40:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/devggbb/phpdebugphpstormplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/devggbb/phpdebugphpstormplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.devggbb.phpdebugphpstormplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.devggbb.phpdebugphpstormplugin.MyBundle
+import com.github.devggbb.phpdebugphpstormplugin.services.MyProjectService
 import javax.swing.JButton
 
 
